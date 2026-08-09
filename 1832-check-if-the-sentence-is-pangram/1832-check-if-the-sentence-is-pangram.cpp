cdf3d1343d40da3150427163ce9ba8cb081e0bec
Artiverma <vermaarti930@gmail.com>
2026-08-09 11:02:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (84.96%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkIfPangram(string sent) {
+        vector <bool> alph(26 , 0);
+        
+        for(int i=0; i<sent.size(); i++){
+            alph[sent[i]-'a']=1;
+        }
+
+       for (int i = 0; i < 26; i++) {
+            if (!alph[i]) return false;
+        }
+        return true;
+    
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
